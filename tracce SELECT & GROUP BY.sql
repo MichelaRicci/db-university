2fecb9614d8db68868bef5d52240c7799f1fcb1b
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-14 17:55:11 +0100

    query2

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -5,6 +5,7 @@ SELECT `name`, `surname`, `date_of_birth` FROM `students` WHERE YEAR(`date_of_bi
 
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+SELECT `name` AS `Corsi`, `cfu` AS `Crediti` FROM `courses` WHERE `cfu` > 10;
 
 
 
