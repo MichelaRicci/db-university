ab9ad883e4d1fa2aa1d5d6689c319b2060b76931
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-14 23:09:19 +0100

    query group1

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -34,9 +34,11 @@ SELECT * FROM `teachers` WHERE `phone` IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(*) AS 'enrolled_students', YEAR(`enrolment_date`) AS 'year' FROM `students` GROUP BY YEAR(`enrolment_date`);
 
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
 
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
