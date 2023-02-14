98b4b48b23cde572f63753610fd7862bf3e9cdc6
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-14 22:42:48 +0100

    query5

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -16,9 +16,10 @@ SELECT `name`, `surname`, `date_of_birth` FROM `students` WHERE YEAR (`date_of_b
 SELECT `name`, `period`, `year` FROM `courses` WHERE `period` LIKE 'I s%' AND `year` = 1;
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-
+SELECT * FROM `exams` WHERE `date`= '2020/06/20' AND `hour`>= '14:00:00';
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
+
 
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
