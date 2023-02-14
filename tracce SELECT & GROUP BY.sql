c9a8059d43a9ca02714a28ee2fd3f81a7536e8fd
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-14 18:10:31 +0100

    query3

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -8,8 +8,8 @@ SELECT `name`, `surname`, `date_of_birth` FROM `students` WHERE YEAR(`date_of_bi
 SELECT `name` AS `Corsi`, `cfu` AS `Crediti` FROM `courses` WHERE `cfu` > 10;
 
 
-
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT `name`, `surname`, `date_of_birth` FROM `students` WHERE YEAR (`date_of_birth`) <= 1992;
 
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
