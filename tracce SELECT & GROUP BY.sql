635399972c52c6dc4fe80e4d637b7dce4260e76e
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-14 17:48:57 +0100

    query1

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -1,9 +1,11 @@
 -- ---------------------------------------  QUERY CON SELECT
 
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
+SELECT `name`, `surname`, `date_of_birth` FROM `students` WHERE YEAR(`date_of_birth`) = 1990;
 
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+
 
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
