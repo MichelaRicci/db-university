029a4933302b9a97f7825cc398c1accc0f1ad9e2
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-14 23:18:26 +0100

    query group3

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -42,6 +42,7 @@ SELECT COUNT(*) AS 'teacher', `office_address` FROM `teachers` GROUP BY (`office
 
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT AVG (`vote`) AS 'average', `exam_id` AS 'exam' FROM `exam_student` GROUP BY (`exam_id`);
 
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
