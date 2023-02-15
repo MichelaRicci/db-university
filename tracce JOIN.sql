9ccbfe59ae88624349e9f8fdc74a3bf737cc4d83
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-15 17:48:29 +0100

    query join3

--- tracce JOIN.sql
+++ tracce JOIN.sql
@@ -17,6 +17,13 @@ WHERE `departments`.`name` = 'dipartimento di neuroscienze';
 
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `courses`.`name`, `teachers`.`id`,`teachers`.`name`, `teachers`.`surname`
+FROM `courses`
+JOIN `course_teacher`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `teachers`.`id` = 44;
 
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
