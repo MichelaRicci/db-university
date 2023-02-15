b419ef6251e45e55d2f2227677de377eb041fb30
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-15 21:20:13 +0100

    query join4

--- tracce JOIN.sql
+++ tracce JOIN.sql
@@ -27,7 +27,13 @@ WHERE `teachers`.`id` = 44;
 
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-
+SELECT DISTINCT `students`.`name`, `students`.`surname`, `degrees`.*, `departments`.`name`
+FROM `students`
+JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname` ASC;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
