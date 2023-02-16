188bd4ad7de6cba86fee18ecdd1a6941e80df057
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-16 16:09:52 +0100

    query join5

--- tracce JOIN.sql
+++ tracce JOIN.sql
@@ -27,7 +27,7 @@ WHERE `teachers`.`id` = 44;
 
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-SELECT DISTINCT `students`.`name`, `students`.`surname`, `degrees`.*, `departments`.`name`
+SELECT `students`.`name`, `students`.`surname`, `degrees`.*, `departments`.`name`
 FROM `students`
 JOIN `degrees`
 ON `degrees`.`id` = `students`.`degree_id`
@@ -35,10 +35,20 @@ JOIN `departments`
 ON `departments`.`id` = `degrees`.`department_id`
 ORDER BY `students`.`surname` ASC;
 
+
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT `courses`.`name`, `degrees`.`name`, `teachers`.`surname`, `teachers`.`name` 
+FROM `degrees` 
+JOIN `courses` 
+ON `degrees`.`id` = `courses`.`degree_id` 
+JOIN `course_teacher` 
+ON `courses`.`id` = `course_teacher`.`course_id` 
+JOIN `teachers` 
+ON `teachers`.`id` = `course_teacher`.`teacher_id`;
 
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
 
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
